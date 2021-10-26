a7ec8541716b695f8c4ecd735a1e6c9e7f71f6cf
Kent Harris <kentharris13@gmail.com>
2021-10-26 14:35:32 -0600

    asdf

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
-    // rollbar.info('html file served successfully')
+    rollbar.info('html file served successfully')
 })
 
 app.get('/style', (req,res) => {
