2f53d5184dffb0fd98a23aaaef83a8c5ade104bf
Kent Harris <kentharris13@gmail.com>
2021-10-26 14:17:27 -0600

    rollbar

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 
+let rollbar = new Rollbar({
+    accessToken:'0f6027f737d5435996a6f0e874b7f1f7',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 const app = express()
 app.use(express.json())
 
@@ -13,6 +19,7 @@ app.get('/style', (req,res) => {
     res.sendFile(path.join(__dirname,'/public/styles.css'))
 })
 
+app.use(rollbar.errorHandler())
 const port = process.env.PORT || 4545
 
 app.listen(port, ()=> console.log(`We have our heading to ${port}!`))
